06c08368f5ab7e12ac7a3e6565216dd4bac97463
Santiago <keycode.santiago@gmail.com>
2023-09-03 19:55:22 -0500

    Fix to print number of odds and evens for each number as well

--- exam1/p3.py
+++ exam1/p3.py
@@ -1,17 +1,27 @@
-evens = 0
-odds = 0
+total_evens = total_odds = 0
+
+print("Enter 0 to finish...\n")
 
 while True:
-    nums = input("Enter a number: ")
-    if nums == "0":
+    num = input("Enter a number: ")
+
+    if num == "0":
         break
-    if not nums.isdigit():
+    if not num.isdigit():
+        print("Invalid input...\n")
         continue
-    for num in nums:
-        if int(num) % 2 == 0:
+
+    evens = odds = 0
+    for digit in num:
+        if int(digit) % 2 == 0:
             evens += 1
         else:
             odds += 1
 
-print(f"number of evens: {evens}")
-print(f"number of odds: {odds}")
+    total_evens += evens
+    total_odds += odds
+    print(f"number of evens: {evens}")
+    print(f"number of odds: {odds}\n")
+
+print(f"total number of evens: {total_evens}")
+print(f"total number of odds: {total_odds}")
